c3cad36e62705c16d5fec5a4c0c48ca60728141a
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-06-21 16:11:09 +0000

    java Script basic

---
+++ basic.js
@@ -0,0 +1,25 @@
+//String in JavaScirpt 
+
+// const name = " zaeem "
+// const repocount = 50 
+
+// console.log(name+repocount+" value ")
+
+// console.log(`My Name is ${name} and MY is Repo Count is ${repocount}`);
+
+const gameName = new String('Zaeem');
+// console.log(gameName)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('m'));
+console.log(gameName.slice(0,3));
+console.log(gameName.substring(2,4));
+const newstring = " HASSAN  "
+console.log(newstring);
+console.log(newstring.trim());
+
+const url = "zaeem_ul_hassan";
+
+console.log(url.replace("_"," "))
+console.log(url.includes("hassan"));
