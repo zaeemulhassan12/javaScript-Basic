eaa2a9b22c32fb3eb7f083526e17ae7ac6263f91
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-06-25 20:06:53 +0000

    Comnpelete Array methods

--- Array.js
+++ Array.js
@@ -1,4 +1,4 @@
-    const arr =[1,2,3,4,5,6,7]
+    // const arr =[1,2,3,4,5,6,7]
 
     // const myarr = arr.slice(1,3)
     // console.log(arr)
@@ -6,10 +6,41 @@
     // const myarr2 = arr.splice(1,3)
     // console.log(arr)
     // console.log(myarr)
-    const myarr3 = arr.push(26)
-    console.log(arr)
-    const myarr4 = arr.pop()
-    const myarr5 = arr.join()
-    console.log(myarr3)
-    console.log(myarr4)
-    console.log(myarr5)
+    // const myarr3 = arr.push(26)
+    // console.log(arr)
+    // const myarr4 = arr.pop()
+    // const myarr5 = arr.join()
+    // console.log(myarr3)
+    // console.log(myarr4)
+    // console.log(myarr5)
+
+    const arr =  ['Zaeem','Ali','hasssan','ahmad','faraz','Tayyab']
+    const arr2 =  ['moiz','','junaid','safi','usman','abdullah']
+
+    // arr.push(arr2)
+
+    // console.log("First array",arr);
+    // const final= arr.concat(arr2)
+
+    // console.log("Second array", arr[6][3]);
+    // console.log("Final array", final[6][3]);
+
+//    //use seperet for array to disturbute arrays 
+//    const S = [...arr ,...arr2]
+//    console.log("separat arrays",S)3
+
+//    const another =[1,2,3,[4,5,6],[7,8,9],[10,11,12,13]]
+//    const F1 =another.flat(Infinity)
+//    console.log(F1)
+
+
+// console.log(Array.isArray('Zaeem'))
+// console.log(Array.from('Zaeem'))
+// console.log(Array.from({name:'Zaeem'}))
+
+let Socre1 =200
+let Socre2 =300
+let Socre3 =500
+let Socre4 =700
+
+console.log(Array.of(Socre1,Socre2,Socre3,Socre4));
