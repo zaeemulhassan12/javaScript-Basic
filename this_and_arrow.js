546c55436fa2639668025aa5381fe5cac46926a8
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-07-02 16:06:22 +0000

    this and arrow or IIFE

---
+++ this_and_arrow.js
@@ -0,0 +1,67 @@
+//const user = {
+//    name:'zaeem',
+//    price: 99,
+//    section:"E",
+//    welcomemessage: function(){
+//        console.log(`${this.name} welcome here`);
+        
+//    }
+//}
+
+// user.welcomemessage()
+
+// user.name ="zaeemulhassan"
+// user.welcomemessage()
+
+// console.log(this);
+
+
+
+// function chai(){
+//     name:"zaeem",
+//     console.log(this);
+    
+// }
+
+// chai()
+
+
+
+
+///
+//  const zaeemi =(num1,num2) => {
+//    return num1+num2
+// }
+
+
+//  const zaeem =(num1,num2) => (num1+num2)
+
+// console.log(zaeemi(1,4));
+// console.log(zaeem(1,5));
+
+
+// Immediately Invoked Function Expressions (IIFE)
+
+(function zaeem(yes){
+    console.log(`${yes}DB CONNECTED`);
+    
+})("he sir welcome in the database");
+
+// zaeem()
+
+
+(function database(){
+
+    
+    console.log(`DB CONNECTED`);
+
+})();
+
+
+
+((YOU)=>{
+
+    
+    console.log(`DB CONNECTED THREE ${YOU}`);
+
+})("WELCOME")
