54aff754550157478643163a019df22311000283
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-07-03 16:06:52 +0000

    for of and for in loops

--- loops.js
+++ loops.js
@@ -27,3 +27,81 @@
 
 
 //High Order Array loops
+//In Array
+
+Array =[1,2,3,4,5,6,7,8,9]
+
+for (const a of Array) {
+    // console.log(`print numbers ${a}`);
+    
+}
+
+//In strings 
+
+
+const Intro ="hello my name is zaerem ul hassan "
+
+
+for (const E of Intro) {
+    // console.log(`Intorduce yourself sir:${E}`);
+    
+}
+
+
+
+//Map
+
+
+// const map =new Map
+
+// map.set('pak','pakistan')
+// map.set('IR','IRAN')
+// map.set('PORT','PORTUGAL')
+// map.set('PORT','PORTUGAL')
+
+// console.log(map);
+
+
+//FOR IN (IN OJBECTS)
+
+const zaeem = {
+
+    name:'zaeem',
+    rollno: 'B-29462',
+
+}
+
+for (const key in zaeem) {
+ 
+    // console.log(`${key} is a ${zaeem[key]}`);
+    //    console.log(key':-'value);
+    
+}
+
+//in array 
+
+
+// Array =[1,2,3,4,5,6,7,8,9]
+
+
+// for (const key in Array) {
+//    console.log(key);
+   
+    
+// }
+
+
+//in Map
+const map = new Map
+map.set('pak','pakistan')
+map.set('IR','IRAN')
+map.set('PORT','PORTUGAL')
+map.set('PORT','PORTUGAL')
+
+
+for (const key in map) {
+    
+    console.log(key);
+    
+}
+
