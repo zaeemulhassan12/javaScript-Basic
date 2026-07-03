a018612c4d341dea60c079fe86e9a090415dab34
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-07-03 15:40:41 +0000

    loops

---
+++ loops.js
@@ -0,0 +1,29 @@
+
+// let index =0
+
+// while(index<=10){
+
+//     console.log(`hy zaeem ${index}`);
+//     index =index+2
+    
+// }
+
+
+
+// let array =["zaeem","usman","Ali","ahmad"]
+// let b=0
+
+// // while(b <array.length) {
+    
+// //     console.log(`hy zaeem ${array[b]}`);
+// //     b=b+ 1
+// // }
+
+// do {
+//         console.log(`hy zaeem ${array[b]}`);
+//         b=b+1
+// } while (b<=array.length);
+
+
+
+//High Order Array loops
