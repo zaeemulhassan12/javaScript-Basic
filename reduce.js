b0c60640356e6b8eb3e59b720930c6923b9477fc
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-07-04 18:49:21 +0000

    Map+filter practice

--- reduce.js
+++ reduce.js
@@ -36,7 +36,10 @@
 array =[1,2,3,4,5,6,7,8,9]
 
 
-const mynum = array.map((num)=>(num*10)).map((num)=>(num+1))
+const mynum = array
+.map((num)=>(num*10))
+.map((num)=>(num+1))
+.filter((num)=>(num>41))
 
 
 console.log(mynum);
