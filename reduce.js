816ae28b0bc9b6e917088c7403e4b166000fe8a8
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-07-04 18:44:07 +0000

    Map practice

--- reduce.js
+++ reduce.js
@@ -1,4 +1,4 @@
-array =[1,2,3,4,5,6,7,8,9]
+// array =[1,2,3,4,5,6,7,8,9]
 
 // const mynum = array.reduce(function(acc,curral){
 //     return acc+curral;
@@ -12,24 +12,31 @@ array =[1,2,3,4,5,6,7,8,9]
 // console.log(mynum);
 
 
-const shoppingcart =[
-    {
-        item:"Data science",
-        price: 444
-    },
-    {
-        item:"Ai",
-        price: 566
-    },    {
-        item:"java Script",
-        price: 888
-    },    {
-        item:"Python",
-        price: 34
-    },
-]
+// const shoppingcart =[
+//     {
+//         item:"Data science",
+//         price: 444
+//     },
+//     {
+//         item:"Ai",
+//         price: 566
+//     },    {
+//         item:"java Script",
+//         price: 888
+//     },    {
+//         item:"Python",
+//         price: 34
+//     },
+// ]
 
 
-const total = shoppingcart.reduce((acc,item)=>(item.price),0)
+// const total = shoppingcart.reduce((acc,item)=>(item.price),0)
 
-console.log(total);
+// console.log(total);
+array =[1,2,3,4,5,6,7,8,9]
+
+
+const mynum = array.map((num)=>(num*10)).map((num)=>(num+1))
+
+
+console.log(mynum);
