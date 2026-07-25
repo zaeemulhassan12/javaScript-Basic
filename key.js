0da829e0620fc657cb8f0daee4a35b019c3c172b
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-07-25 22:26:58 +0000

    Keyboard project

---
+++ key.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `<div class='color'>
+  <table>
+  <tr>
+    <th>key</th>
+    <th>Keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key == ' ' ? 'space' : e.key}</td>
+    <td>${e.keycode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+  </div>`;
+});
