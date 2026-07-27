bfa1e40c9683a9c4969cce6681d6bef722aa9a2e
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-07-27 19:53:12 +0000

    promises done

--- promises.js
+++ promises.js
@@ -42,29 +42,84 @@
 // })
 
 
-const PromiseFour = new Promise(function(reslve ,reject){
-    let error =false;
-    if(!error){
-        reslve({Username:'Zaeem', Password :'Paistan123'})
-    }else(
-        reject('Error is Occur please see this')
-    )
-})
+// const PromiseFour = new Promise(function(reslve ,reject){
+//  setTimeout(function(){
+//        let error =false;
+//     if(!error){
+//         reslve({Username:'Zaeem', Password :'Paistan123'})
+//     }else(
+//         reject('Error is Occur please see this')
+//     )
+//  },1000)
+// })
 
-PromiseFour
-.then(function(user){
-    console.log(user);
-    return user.Username;
+// PromiseFour
+// .then(function(user){
+//     console.log(user);
+//     return user.Username;
     
-})
-.then(function(Username){
-console.log(Username);
+// })
+// .then(function(Username){
+// console.log(Username);
 
-})
-.catch(function(error){
-    console.log(error);
+// })
+// .catch(function(error){
+//     console.log(error);
     
-}).finally(function(){
-    console.log('all code is done is resolve or Rejected');
+// }).finally(function(){
+//     console.log('all code is done is resolve or Rejected');
     
-})
+// })
+
+// const PromiseFive = new Promise(function(reslve ,reject){
+//     setTimeout(function(){
+//        let error =false;
+//     if(!error){
+//         reslve({Username:'zafar', Password :'Paistan123'})
+//     }else(
+//         reject('Error is JS IS WENT WRONGss')
+//     )
+//  },3000)
+
+// })
+
+// async function consumedPromies() {
+//  try{
+//     const response = await PromiseFive;
+//     console.log(response);
+//  } catch(error){
+//     console.log(error);   
+//  }   
+// }
+
+
+// consumedPromies()
+
+
+// async function getallusers(params) {
+//   try {
+//    const response = await fetch('https://randomuser.me/api/')
+//   const data = await response.json()
+//   console.log(data);
+   
+//   } catch (error) {
+//    console.log('Error is Ouccr',error);
+   
+//   }
+  
+// }
+
+// getallusers()
+
+
+
+
+fetch('https://randomuser.me/api/').then(function(response){
+   return response.json()
+}).then(function(data){
+   console.log(data);
+   
+}).catch(function(error){
+console.log('Something went wrong ', error);
+
+});
